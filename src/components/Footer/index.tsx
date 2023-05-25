30b04618d5d719a21c27a1336a098b7584663aa0
Polyana-Rangel <polyanarangel17@gmail.com>
2023-05-24 23:31:53 -0300

    feat: added footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import Font from "../Font";
+import { Container } from "./styles";
+
+function Footer() {
+  return (
+    <Container>
+      <Font size="14px" weight="400">Página teste Envolve - Todos os Direitos Resevados</Font>
+    </Container>
+   
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 0 2rem 2.3rem 2rem;
+
+
+`
+
